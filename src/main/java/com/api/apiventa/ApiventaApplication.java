9d98da59938bfb0b4be609dcf2b1499fc1938f17
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 23:13:43 -0400

    Base API Venta

---
+++ src/main/java/com/api/apiventa/ApiventaApplication.java
@@ -0,0 +1,13 @@
+package com.api.apiventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiventaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apiventa/ApiventaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
