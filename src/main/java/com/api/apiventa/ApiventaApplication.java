592d708519f4c6291eb5300c8a2a6c8c023c220c
ErasmoJimenez <erajimenezg@gmail.com>
2025-06-08 21:16:11 -0400

    Cambio en main para escaneo de carpetas

--- src/main/java/com/api/apiventa/ApiventaApplication.java
+++ src/main/java/com/api/apiventa/ApiventaApplication.java
@@ -3,7 +3,7 @@ package com.api.apiventa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.api.apiventa")
 public class ApiventaApplication {
 
 	public static void main(String[] args) {
